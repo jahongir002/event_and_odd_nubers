f21e845653ec1bd364bff19df0c4e42f308d1e75
jahongir <jahongirabdalimov02@gmail.com>
2023-05-23 14:47:40 +0500

    number_even.py

--- is_even.py
+++ is_even.py
@@ -1,5 +1,5 @@
 #Create a variable "number" and assign it a intrger value.
 def main(number):
-    return (number%2+1)%2
+    return (number+1)%2
 #Print 1 if the number is even, otherwise print 0.
 print(main(13))
--- number_even.py
+++ number_even.py
@@ -1,5 +1,7 @@
 #A four-digit integer is given. Find the number of even digits in it.
-
+var_int=4586
 #Create a variable "var_int" and assign it a four-digit integer value.
-
+def main(var_int):
+    return (var_int // 1000 + 1)%2 + (var_int // 100 +1)%2 + (var_int // 10 +1)%2 + (var_int%2+1)%2
 #Print the number of even digits in the variable "var_int".
+print(main(4586))
