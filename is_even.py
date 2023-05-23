98e91e21ff2a12b2eba638e0b24018dc87836c80
jahongir <jahongirabdalimov02@gmail.com>
2023-05-23 13:12:23 +0500

    is_even.py

--- is_even.py
+++ is_even.py
@@ -1,3 +1,5 @@
-#Create a variable "number" and assign it a integer value.
-
+#Create a variable "number" and assign it a intrger value.
+def main(number):
+    return number%3
 #Print 1 if the number is even, otherwise print 0.
+print(main(7))
